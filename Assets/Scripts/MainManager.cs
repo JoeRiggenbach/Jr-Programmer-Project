2e7a235519772b97437acc7dc189227eb6b9d4ea
JoeRiggenbach <joeyriggenbach@gmail.com>
2023-12-01 19:29:19 -0600

    Forgot a semicolon

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,7 +5,7 @@ using System.IO;
 public class MainManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static MainManager Instance;
+    public static MainManager Instance { get; private set; }
     public Color TeamColor;
     private void Awake()
     {
